342c862dad127370b963f27c6a6ba90a2b8ba15e
stharuma <stharuma@gmail.com>
2019-06-21 10:36:00 -0400

    updated host-add

--- client/src/app/host/host-add.component.ts
+++ client/src/app/host/host-add.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { BackendService } from "../services/backend.service";
 
 @Component({
-  selector: 'app-host-add',
-  templateUrl: './host-add.component.html',
-  styleUrls: ['./host-add.component.css']
+  selector: "app-host-add",
+  templateUrl: "./host-add.component.html",
+  styleUrls: ["./host-add.component.css"]
 })
-export class HostAddComponent implements OnInit {
+export class HostAddComponent implements OnInit, OnDestroy {
+  error: boolean = false;
+  errorMessage: String = "Something went wrong with App";
+  dataLoading: boolean = false;
+  private querySubscription;
+  savedChanges: boolean = false;
 
-  constructor() { }
+  constructor(private _backendService: BackendService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  setHost(formData) {
+    this.dataLoading = true;
+    this.querySubscription = this._backendService.setHost(formData).subscribe(
+      res => {
+        if (res["errorCode"] > 0) {
+          this.error = false;
+          this.errorMessage = "";
+          this.dataLoading = false;
+          this.savedChanges = true;
+        } else {
+          this.error = true;
+          this.errorMessage = res["errorMessage"];
+          this.dataLoading = false;
+        }
+      },
+      error => {
+        this.error = true;
+        this.errorMessage = error.message;
+        this.dataLoading = false;
+      },
+      () => {
+        this.dataLoading = false;
+      }
+    );
   }
 
+  ngOnDestroy() {
+    if (this.querySubscription) {
+      this.querySubscription.unsubscribe();
+    }
+  }
 }
