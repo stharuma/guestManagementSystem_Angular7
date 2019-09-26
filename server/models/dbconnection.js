2304816dae3b47e76a8d47f7333dcad98473da98
stharuma <stharuma@gmail.com>
2019-09-26 12:23:07 -0400

    added and commented DB connection err

--- server/models/dbconnection.js
+++ server/models/dbconnection.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 var mongoDB = process.env.mongoDBURL;
 mongoose.connect(mongoDB);
 
+// mongoose.connection.on('error', (err)=>{
+//     if(err){
+//         console.log("Error in database connection "+err);
+//     }
+// });
+
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 mongoose.connection.once('open', () => {
     console.log('conneted to MONGODB- visitor_app database');
